c7a5ab2d061b6bf04f67fe64441a5ee0c4b13453
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-02-10 00:55:38 +0100

    beerchat compat
    beerchat mute/ignore disallows inviting too

--- init.lua
+++ init.lua
@@ -275,7 +275,12 @@ function spectator_mode.is_permited_to_invite(name_target, name_watcher)
 		return true
 	end
 
-	-- TODO: check chat and tpr mute if the mods are active
+	-- check for beerchat mute/ignore
+	local meta = get_player_by_name(name_watcher):get_meta()
+	if 'true' == meta:get_string('beerchat:muted:' .. name_target) then
+		return false
+	end
+
 	return false
 end
 -- luacheck: unused args
